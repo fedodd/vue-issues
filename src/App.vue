a735e114b782f0aee2a7f3a48a769ca73321780a
fedodd <fedodd.s@gmail.com>
2020-03-06 10:53:21 +0300

    add pagination

--- src/App.vue
+++ src/App.vue
@@ -4,50 +4,60 @@
     <h1>
       {{name}}
     </h1>
-    <button v-on:click="getData">Загрузить</button>
+    <button v-on:click="getData(link)">Загрузить</button>
     <!-- <ol>
       <li v-for="issue in issues" v-bind:key="issue.id">
         {{ issue.title }}
       </li>
     </ol> -->
     <Issues v-bind:issues-data="issues"></Issues>
+    <Pagination v-bind:pageLinks="pageLinks" v-bind:getData="getData"/>
   </div>
 </template>
 
 <script lang="ts">
   import axios from 'axios';
   import Vue from "vue";
+  import parse from 'github-parse-link';
   import Issues from './components/issues/Issues';
+  import Pagination from "./components/Pagination";
+
 
   export default Vue.extend({
     data: function() {
       return {
         name: 'Список открытых задач по репозеторию vue:',
-        issues: []
+        issues: [],
+        page: 1,
+        pageLinks: {},
+        link: 'https://api.github.com/repos/vuejs/vue/issues',
+        linkParams: {
+          state: "open",
+          sort: "comments",
+          page: 1
+        }
       }
     },
     methods: {
-      getData: function (e) {
+      getData: function (link, linkParams) {
         async function getIssue(callback) {
           try {
-            const response = await axios.get('https://api.github.com/repos/vuejs/vue/issues', {
-              params: {
-                state: "open",
-                sort: "comments"
-              }
-            });
-            callback(response.data);
-
+            const response = await axios.get(link, {params: {...linkParams}});
+            callback(response);
           } catch (error) {
             console.error(error);
           }
         }
-
-        getIssue((data) => this.issues = data);
-      }
+        getIssue((response) => {
+          const {first, prev, next, last} = parse(response.headers.link);
+          this.pageLinks = {first, prev, next, last}
+          this.issues = response.data
+        });
+      },
     },
     components: {
-      Issues
+      Issues,
+      Pagination
     }
   });
 </script>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button
+      v-for="(link, buttonType) in pageLinks"
+      v-bind:key="buttonType"
+      v-bind:disabled="link===undefined"
+      @click="getData(link)" >{{buttonType}}</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend({
+    props: {
+      'getData': Function,
+      'pageLinks': Object
+    }
+  });
+</script>
+
+
+<style>
+
+
+</style>
