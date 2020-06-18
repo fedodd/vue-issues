3314732c7a4b63c5979302d237eece30a1a4b47d
fedodd <fedodd.s@gmail.com>
2020-06-18 08:20:38 +0300

    fix style bug after merge

--- src/App.vue
+++ src/App.vue
@@ -5,106 +5,100 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
+import Vue from "vue";
 
-  export default Vue.extend({});
+export default Vue.extend({});
 </script>
 
 <style>
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    margin: 0;
-  }
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
 
-  h1 {
-    width: max-content;
-    max-width: 70vw;
-  }
+h2 {
+  width: max-content;
+  max-width: 70vw;
+}
 
-  .holder {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+.holder {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .button {
-    background-color: darkmagenta;
-    color: white;
-    border: none;
-    border-radius: 2px;
-    padding: 5px;
-    margin-right: 20px;
-    transition: 0.3s;
-    outline: none;
-  }
+.button {
+  background-color: darkmagenta;
+  color: white;
+  border: none;
+  border-radius: 2px;
+  padding: 5px;
+  margin-right: 20px;
+  transition: 0.3s;
+  outline: none;
+}
 
-  .button:hover {
-    cursor: pointer;
-    box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.75);
-  }
+.button:hover {
+  cursor: pointer;
+  box-shadow: 2px 3px 5px 1px rgba(0, 0, 0, 0.75);
+}
 
-  .button:hover:disabled {
-    cursor: not-allowed;
-    box-shadow: none;
-  }
+.button:hover:disabled {
+  cursor: not-allowed;
+  box-shadow: none;
+}
 
-  .button:last-child {
-    margin-right: 0;
-  }
+.button:last-child {
+  margin-right: 0;
+}
 
-  .button:disabled {
-    background-color: grey;
-    color: lightgrey;
-  }
+.button:disabled {
+  background-color: grey;
+  color: lightgrey;
+}
 
-  .button.is__sort {
-    position: relative;
-    display: inline-block;
-    position: relative;
-    bottom: 10px;
-    margin-bottom: -10px;
-    opacity: 0.3;
-    width: 20px;
-    height: 20px;
+.button.is__sort {
+  position: relative;
+  display: inline-block;
+  position: relative;
+  bottom: 10px;
+  margin-bottom: -10px;
+  opacity: 0.3;
+  width: 20px;
+  height: 20px;
+  box-sizing: border-box;
+  border: 10px solid transparent;
+  border-bottom-color: darkmagenta;
+  background-color: transparent;
+  padding: 0;
+}
+
+.button.is__sort.is__active {
+  opacity: 1;
+}
+
+.is__sort.is__toggled {
+  transition: 0.3s;
+  bottom: 0;
+  transform: rotate(180deg);
+}
+
+.button.is__sort:hover {
+  box-shadow: none;
+  opacity: 1;
+}
+
+@media (max-width: 400px) {
+  h2 {
+    font-size: 1.2em;
     box-sizing: border-box;
-    border: 10px solid transparent;
-    border-bottom-color: darkmagenta;
-    background-color: transparent;
-    padding: 0;
+    padding: 5px 10px;
+    max-width: 80vw;
+    text-align: center;
+    margin: 0 auto;
   }
-
-  .button.is__filter.is__active {
-    opacity: 1;
+  .holder {
+    margin: 5px;
   }
-
-  .is__filter.is__toggled {
-    transition: 0.3s;
-    bottom: 0;
-    transform: rotate(180deg);
-  }
-
-  .button.is__sort:hover {
-    box-shadow: none;
-    opacity: 1;
-  }
-
-  .button.is__sort.is__active {
-    opacity: 1;
-  }
-
-  .button.is__sort.is__reverse {
-    top: -10px;
-    transform: rotate(180deg);
-  }
-
-  @media (max-width: 400px) {
-    h1 {
-      font-size: 1.2em;
-      width: 100%;
-      padding: 5px;
-    }
-    .holder {
-      margin: 5px;
-    }
-  }
+}
 </style>
--- src/components/issueTable/IssueTable.vue
+++ src/components/issueTable/IssueTable.vue
@@ -7,7 +7,7 @@
         Ком&shy;мент.
         <div class="issue-buttonWrapper">
           <button
-            class="button is__filter"
+            class="button is__sort"
             v-bind:class="{
               is__toggled: sortComments.dir === 'asc',
               is__active: sortComments.isActive,
@@ -20,7 +20,7 @@
         Создана
         <div class="issue-buttonWrapper">
           <button
-            class="button is__filter"
+            class="button is__sort"
             v-bind:class="{
               is__toggled: sortCreated.dir === 'asc',
               is__active: sortCreated.isActive,
@@ -30,113 +30,108 @@
         </div>
       </th>
     </thead>
-    <Issue
-      v-for="issue in issuesData"
-      v-bind:key="issue.id"
-      v-bind:issue="issue"
-    />
+    <Issue v-for="issue in issuesData" v-bind:key="issue.id" v-bind:issue="issue" />
   </table>
 </template>
 
 <script>
-  import Vue from 'vue';
-  import Issue from './issue/Issue';
+import Vue from "vue";
+import Issue from "./issue/Issue";
 
-  export default Vue.extend({
-    name: 'IssueTable',
-    data: function() {
-      return {
-        sortComments: {
-          dir: 'desc',
-          isActive: false,
-        },
-        sortCreated: {
-          dir: 'asc',
-          isActive: true,
-        },
-      };
-    },
-    props: {
-      issuesData: Array,
-      getData: Function,
-      link: String,
-    },
-    methods: {
-      sortCommentsHandler: function() {
-        this.getData(this.link, {
-          sort: 'comments',
-          direction: this.sortComments.dir,
-        });
-        this.sortComments.isActive = true;
-        this.sortCreated.isActive = false;
-        this.sortComments.dir =
-          this.sortComments.dir === 'desc' ? 'asc' : 'desc';
+export default Vue.extend({
+  name: "IssueTable",
+  data: function() {
+    return {
+      sortComments: {
+        dir: "desc",
+        isActive: false
       },
-      sortCreatedHandler: function() {
-        this.getData(this.link, {
-          sort: 'created_at',
-          direction: this.sortCreated.dir,
-        });
-        this.sortComments.isActive = false;
-        this.sortCreated.isActive = true;
-        this.sortCreated.dir = this.sortCreated.dir === 'desc' ? 'asc' : 'desc';
+      sortCreated: {
+        dir: "asc",
+        isActive: true
       }
+    };
+  },
+  props: {
+    issuesData: Array,
+    getData: Function,
+    link: String
+  },
+  methods: {
+    sortCommentsHandler: function() {
+      this.getData(this.link, {
+        sort: "comments",
+        direction: this.sortComments.dir
+      });
+      this.sortComments.isActive = true;
+      this.sortCreated.isActive = false;
+      this.sortComments.dir = this.sortComments.dir === "desc" ? "asc" : "desc";
     },
-    components: {
-      Issue,
-    },
-  });
+    sortCreatedHandler: function() {
+      this.getData(this.link, {
+        sort: "created_at",
+        direction: this.sortCreated.dir
+      });
+      this.sortComments.isActive = false;
+      this.sortCreated.isActive = true;
+      this.sortCreated.dir = this.sortCreated.dir === "desc" ? "asc" : "desc";
+    }
+  },
+  components: {
+    Issue
+  }
+});
 </script>
 
 <style>
-  .table {
-    border-collapse: collapse;
-    max-width: 100%;
-  }
+.table {
+  border-collapse: collapse;
+  max-width: 100%;
+}
 
+.th,
+.td {
+  padding: 5px 15px 5px 5px;
+}
+
+.th {
+  position: relative;
+}
+
+.tr:nth-child(2n + 1) {
+  background-color: #efefef;
+}
+
+.issue-name {
+  max-width: 500px;
+}
+
+.issue-comments {
+  text-align: right;
+}
+
+.issue-status {
+  text-align: center;
+}
+
+.issue-created {
+  text-align: right;
+}
+
+.issue-buttonWrapper {
+  position: relative;
+  display: inline-block;
+}
+
+@media (max-width: 400px) {
   .th,
   .td {
-    padding: 5px 15px 5px 5px;
+    font-size: 0.8em;
+    padding: 4px 5px 4px 2px;
   }
 
   .th {
-    position: relative;
+    vertical-align: bottom;
   }
-
-  .tr:nth-child(2n + 1) {
-    background-color: #efefef;
-  }
-
-  .issue-name {
-    max-width: 500px;
-  }
-
-  .issue-comments {
-    text-align: right;
-  }
-
-  .issue-status {
-    text-align: center;
-  }
-
-  .issue-created {
-    text-align: right;
-  }
-
-  .issue-buttonWrapper {
-    position: relative;
-    display: inline-block;
-  }
-
-  @media (max-width: 400px) {
-    .th,
-    .td {
-      font-size: 0.8em;
-      padding: 4px 5px 4px 2px;
-    }
-
-    .th {
-      vertical-align: bottom;
-    }
-  }
+}
 </style>
